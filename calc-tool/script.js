569f6d4c4b3b3a36d58ab78e2ea35c426f323e17
ll <3544067586@qq.com>
2026-09-10 10:47:54 +0800

    添加成绩数据定义

---
+++ calc-tool/script.js
@@ -0,0 +1,10 @@
+const scores = [
+  { name: '高数', score: 92 },
+  { name: '大物', score: 45 },
+  { name: '模电', score: 77 },
+  { name: 'C语言', score: 59 },
+  { name: '思想品德', score: 88 },
+  { name: '英语', score: 105 }, 
+  { name: '中文写作', score: -3 }
+];
+console.table(scores)
